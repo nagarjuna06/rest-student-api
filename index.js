a4863d0b9bfbf9feb18e86884b824eef65b70fd9
nagarjuna <chennanagarjuna4gmail.com>
2023-03-08 19:26:15 +0530

    add cors package

--- index.js
+++ index.js
@@ -2,21 +2,12 @@ const express = require('express');
 const path = require('path');
 const { open } = require('sqlite');
 const sqlite3 = require('sqlite3');
+const cors = require('cors')
 const app = express();
 app.use(express.json());
-app.use(cors());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Methods",
-        "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-    );
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin,X-Requested-With,Content-Type,Accept,Authorization"
-    );
-    next();
-})
+app.use(cors({
+    origin: 'http://localhost:3000/'
+}));
 const dbPath = path.join(__dirname, "student.db")
 let db = null
 const port = process.env.PORT || 3000
