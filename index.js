8d0c91110109e49272fcabab62ad85940a33a6aa
nagarjuna <chennanagarjuna4gmail.com>
2023-03-08 19:44:20 +0530

    change port number

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000/'
+    origin: 'http://localhost:5500/'
 }));
 const dbPath = path.join(__dirname, "student.db")
 let db = null
