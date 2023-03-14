45cd0592d48c62a769f7fff418b35cb55118c6f2
nagarjuna <chennanagarjuna4gmail.com>
2023-03-14 21:33:30 +0530

    compression

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const path = require('path');
 const { open } = require('sqlite');
 const sqlite3 = require('sqlite3');
 const cors = require('cors');
+// const compression = require('compression');
 const app = express();
 app.use(express.json());
+// app.use(compression())
 app.use(cors({
     origin: 'http://127.0.0.1:5500'
 }));
