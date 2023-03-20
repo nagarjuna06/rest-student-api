fcca7ca22ee9dc28476a4b69388efffafa0d0ee9
nagarjuna <chennanagarjuna4gmail.com>
2023-03-20 08:19:54 +0530

    database changed

--- index.js
+++ index.js
@@ -1,62 +1,56 @@
 const express = require('express');
-const path = require('path');
-const { open } = require('sqlite');
-const sqlite3 = require('sqlite3');
+const { Pool } = require('pg');
 const cors = require('cors');
-// const compression = require('compression');
+
 const app = express();
 app.use(express.json());
-// app.use(compression())
 app.use(cors());
-const dbPath = path.join(__dirname, "student.db")
-let db = null
-const port = process.env.PORT || 3000
-const initializeDBandServer = async () => {
-    try {
-        db = await open({
-            filename: dbPath,
-            driver: sqlite3.Database
-        })
-        app.listen(port, () => console.log(`Server is running at http://localhost:${port}/`))
-    }
-    catch (e) {
-        console.log(`DB Error : ${e.message}`);
-        process.exit(1)
-    }
-}
-initializeDBandServer()
 
-//this is the origin
-const isThisOrigin = (request, response, next) => {
-    const { origin } = request.headers;
-    if (origin === 'http://192.168.43.143:3000') {
-        next()
-    }
-    else {
-        response.status(400)
-        response.send({ msg: " This Api has Restricted by Admin!" })
-    }
-}
+const db = new Pool({
+    connectionString: 'postgres://admin:eWVvSRAMe4wkCbNkIBTcuDhBGHIpvwWu@dpg-cgbe7hl269v4icrcvid0-a/testing_g1yp',
+    ssl: { rejectUnauthorized: false }
+});
+
+
+app.listen(3000, () => console.log('server is running at http://localhost:3000'))
 
 //get all students details
-app.get('/students', async (request, response) => {
-    const query = `SELECT * FROM student`
-    const Array = await db.all(query)
-    response.send(Array)
+app.get('/students', (request, response) => {
+    const query = `SELECT * FROM student order by adm_no;`
+    db.query(query, (err, res) => {
+        if (err) {
+            response.send('error!')
+        }
+        else {
+            response.send(res.rows)
+        }
+    })
 })
 
 //get student by id
-app.get('/students/:studentId/', async (request, response) => {
+0
+app.get('/students/:studentId/', (request, response) => {
     const { studentId } = request.params
     const getStudentQuery = `SELECT * FROM STUDENT WHERE adm_no=${studentId}`;
-    const Array = await db.get(getStudentQuery)
-    response.send(Array)
+    db.query(getStudentQuery, (err, res) => {
+        if (err) {
+            response.send('error!')
+        }
+        else {
+            if (res.rows.length > 0) {
+                response.send(res.rows)
+            }
+            else {
+                response.send(`${studentId} not found!`)
+            }
+        }
+    })
 })
 
 
 
 //add student
-app.post('/students', async (request, response) => {
+app.post('/students', (request, response) => {
     const studentDetails = request.body
     const { name, course, deptName, mobileNo } = studentDetails
     const addStudentQuery = `
@@ -65,16 +59,21 @@ app.post('/students', async (request, response) => {
         '${name}',
         '${course}',
         '${deptName}',
-        ${mobileNo}
-    )
-    `;
-    const DbResponse = await db.run(addStudentQuery);
-    const studentId = DbResponse.lastID;
-    response.send({ studentId })
+        '${mobileNo}'
+    ) RETURNING adm_no
+    ;`;
+    db.query(addStudentQuery, (err, res) => {
+        if (err) {
+            response.send('error!')
+        }
+        else {
+            response.send(`Your Admission number is ${res.rows[0].adm_no} !`)
+        }
+    })
 })
 
 //update student details
-app.put('/students', async (request, response) => {
+app.put('/students', (request, response) => {
     const studentDetails = request.body
     const { admNo, name, course, deptName, mobileNo } = studentDetails
     const updateStudentQuery = `
@@ -83,29 +82,36 @@ app.put('/students', async (request, response) => {
     name='${name}',
     course='${course}',
     dept_name='${deptName}',
-    mobile_no=${mobileNo}
+    mobile_no='${mobileNo}'
     WHERE
     adm_no=${admNo}
     `
-    await db.run(updateStudentQuery)
-    response.send(`${admNo} has updated successfully!`)
+    db.query(updateStudentQuery, (err, res) => {
+        if (err) {
+            response.send('error!')
+        }
+        else {
+            response.send(`${admNo} has updated successfully!`)
+        }
+    })
 })
 
 //delete student
-app.delete('/students/:studentId', async (request, response) => {
+app.delete('/students/:studentId', (request, response) => {
     const { studentId } = request.params
     const deleteStudentQuery = `DELETE FROM STUDENT WHERE adm_no=${studentId};`;
-    await db.run(deleteStudentQuery)
-    response.send(`${studentId} deleted successfully!`)
+    db.query(deleteStudentQuery, (err, res) => {
+        if (err) {
+            response.send('error!')
+        }
+        else {
+            response.send(`${studentId} deleted successfully!`)
+        }
+    })
 })
 
-app.get('/embed', async (request, response) => {
-    const res = await fetch('https://oembed.com/providers.json');
-    const result = await res.json()
-    response.send(result)
-})
 
-app.get('/', async (request, response) => {
+app.get('/', (request, response) => {
     response.status(200);
     response.send("welcome")
 })
